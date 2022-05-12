269ab2b1e50c4bd3c9d3363192e6b7d64c5b4d9b
Ding_Hong_Chen <daniel23232326@gmai.com>
2022-05-12 20:15:33 +0800

    換表單樣式

--- cms/views.py
+++ cms/views.py
@@ -16,7 +16,7 @@ def reserve_detail(request):
     # 取出時間大於當前的資料供cms檢視
     # __lte, __gte, __lt, __gt are used for <=, >=, < and >
     reserve_details = reserve_inform.objects.filter(
-        reserve_datetime_confirm__lte=datetime.now())
+        reserve_datetime_confirm__gte=datetime.now())
     context = {
         'dateTimeNow': datetime.now(),#比較用
         'reserve_details': reserve_details,
