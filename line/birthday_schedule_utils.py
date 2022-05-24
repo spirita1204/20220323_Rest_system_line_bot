d14dfeb270642690c3dac78efb0787a55472cafc
Ding_Hong_Chen <daniel23232326@gmai.com>
2022-05-24 16:06:29 +0800

    rest_info改變

--- line/birthday_schedule_utils.py
+++ line/birthday_schedule_utils.py
@@ -1,13 +1,14 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
-
+print("ready")
 sched.start()
+print("start")
